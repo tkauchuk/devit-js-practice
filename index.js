232702e86eb414a555fe0217d03c4562367c0ed7
Alexander Tkachuk <tkachuka2020@gmail.com>
2021-08-03 22:30:11 +0300

    game heroes classes created

--- index.js
+++ index.js
@@ -1,34 +1,74 @@
-class Car {
-    static WHEELS_COUNT = 4;
 
-    constructor(type, wheelPrice) {
-        this.property = 123;
-        this.type = type;
-        this.wheelPrice = wheelPrice;
+// game characters 
+
+class Hero {
+    static description = 'Class that describes characteristics of a hero';
+
+    static strengthComparison(value, magnitude) {
+        return `First Hero is stronger than another one by ${value - magnitude} points`;
     }
 
-    static myMethod() {
-        return this.type;
+    constructor({ name, level, dexterity, vitality } = {}) {
+        this._name = name;
+        this._level = level;
+        this._dexterity = dexterity;
+        this._vitality = vitality;
     }
 
-    getWheelsPrice() {
-        return Car.WHEELS_COUNT * this.wheelPrice;
+    get level() {
+        return this._level;
     }
 
-    getType() {
-        return this.type;
-    }
-};
-
-class ElectroCar extends Car {
-    constructor(wheelPrice) {
-        super("electro", wheelPrice);
+    set level(value) {
+        this._level = value;
     }
 }
 
+class Magician extends Hero {
+    constructor({ magic, ...restProps } = {}) {
+        super(restProps);
 
-const car2 = new ElectroCar(50);
-const car3 = new ElectroCar(500);
+        this.magic = magic
+    }
+}
 
-console.log(car2.getWheelsPrice());
-console.log(car3.getWheelsPrice());
+class Warrior extends Hero {
+    constructor({strength, ...restProps} = {}) {
+        super(restProps);
+
+        this.strength = strength;
+    }
+
+    showsHisStrength() {
+        return `Warrior ${this.name} has strength of ${this.strength} points`;
+    }
+}
+
+class Barbarian extends Warrior {
+    constructor({ weapon, ...restProps } = {}) {
+        super(restProps);
+
+        this.weapon = weapon;
+    }
+
+    attacks() {
+        return `Barbarian ${this.name} attacks using ${this.weapon}`;
+    }
+}
+
+const boris = new Barbarian({
+    name: 'Boris',
+    level: 3,
+    dexterity: 40,
+    vitality: 75,
+    strength: 60,
+    weapon: 'sword'
+});
+
+console.log(Hero.description);
+console.log(Hero.strengthComparison(100, 10));
+
+console.log(boris);
+console.log(boris.attacks());
+console.log(boris.level);
+console.log(boris.showsHisStrength());
