800682d47a30cf36f4d4c48bb8541610da848498
tkauchuk <tkachuka2020@gmail.com>
2021-08-06 16:06:28 +0300

    async functions added

--- index.js
+++ index.js
@@ -50,13 +50,50 @@ async function makeRequest(index) {
 const queueOfPrioritisedRequests = new PrioritisedQueue();
 const requestsQuantity = 100;
 
-for (let i = 0; i < requestsQuantity; i++) {
-    queueOfPrioritisedRequests
-        .add(() => makeRequest(i), i)
-        .then((result) => {
-            console.log(result);
-        })
-        .catch((error) => {
-            console.error(error.message);
-        })
+// for (let i = 0; i < requestsQuantity; i++) {
+//     queueOfPrioritisedRequests
+//         .add(() => makeRequest(i), i)
+//         .then((result) => {
+//             console.log(result);
+//         })
+//         .catch((error) => {
+//             console.error(error.message);
+//         })
+// }
+const makeAnArray = (length) => {
+    let array = [];
+    for (let i = 1; i <= length; i++) {
+        array.push(i);
+    }
+    return array;
 }
+
+const fetchData = async (id) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    return await response.json();
+}
+
+const asyncIterations = async (count) => {
+    const usersQuantity = makeAnArray(3);
+    const userIds = usersQuantity.map(userId => fetchData(userId));
+    return await Promise.all(userIds);
+};
+
+const syncIterations = async (id) => {
+    let results = [];
+    for (let i = 0; i < 5; i++) {
+        results.push(await asyncIterations(3));
+    }
+
+    return results;
+}
+
+const multipleFetch = async (ids) => {
+    const fetchResults = ids.map(id => syncIterations(id));
+    return await Promise.all(fetchResults);
+}
+
+const multipleFetchResult = await multipleFetch(makeAnArray(10));
+
+console.log(multipleFetchResult);
+
